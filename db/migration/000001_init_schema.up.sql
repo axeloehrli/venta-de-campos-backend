4c639461725f1b0bc4bb2ade7e7a92f2f8d41748
axeloehrli <axel.oehrli@gmail.com>
2022-10-27 01:54:08 -0300

    Added unique constraints to username and email fields of users table

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "usuarios" (
   "id" bigserial PRIMARY KEY NOT NULL,
-  "nombre_usuario" varchar NOT NULL,
+  "nombre_usuario" varchar UNIQUE NOT NULL,
   "hashed_password" varchar NOT NULL,
   "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
   "nombre" varchar NOT NULL,
   "apellido" varchar NOT NULL,
-  "email" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
   "fecha_creacion" timestamptz NOT NULL DEFAULT (now())
 );
 
